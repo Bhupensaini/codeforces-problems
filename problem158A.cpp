83270d6ecabf6a8788a27983b6fe82ac53c43f31
Bhupen saini <63636192+Bhupensaini@users.noreply.github.com>
2021-06-18 09:51:16 +0530

    Add files via upload

---
+++ problem158A.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,k;
+	cin>>n>>k;
+	int val = 0;
+	int arr[n];
+	int x=0;
+	
+	for (int i=0; i < n; i++) {
+		cin>>arr[i];
+    }
+
+	for (int i=0; i < n; i++) {
+		if ( arr[i] >= arr[k-1] and arr[i] != 0 ){
+			val++;
+		} 
+    }
+	cout<<val;
+	return 0;
+}
